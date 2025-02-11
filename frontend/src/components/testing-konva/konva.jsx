70b357c47148bab874c9ba287ad673c913709244
andrew-adomaitis <theelderone02@gmail.com>
2025-02-11 14:18:54 -0500

    Created basic grid
    Will need more expansion if we want to use it for more uses

--- frontend/src/components/testing-konva/konva.jsx
+++ frontend/src/components/testing-konva/konva.jsx
@@ -1,30 +1,57 @@
 import { 
     Stage, 
-    Text, 
     Layer, 
     Rect 
 } from 'react-konva';
+import './konva.css'
 
 const KonvaTest = () => {
-    return (
-        <Stage width={400} height={400}>
-            <Layer>
-                <Text
-                    text='Konva Canvas' 
-                    fontSize={50}
-                    fill='black'
+    const rows = 10;
+    const cols = 10;
+    const width = 500;
+    const height = 500;
+
+    const cellWidth = width / cols;
+    const cellHeight = height / rows;
+
+    const gridRects = [];
+
+    // Generate the grid 
+    for(let row = 0; row < rows; row++) {
+        for(let col = 0; col < cols; col++) {
+            gridRects.push(
+                <Rect 
+                    key={`row, col`}
+                    x={col*cellWidth}
+                    y={row*cellHeight}
+                    width={cellWidth}
+                    height={cellHeight}
+                    stroke='black'
+                    strokeWidth={.1}
                 />
+            )
+        }
+    }
+
+    return (
+        <Stage className='canvas' width={width} height={height}>
+            <Layer>
+                {/* Border */}
                 <Rect
-                    x={200}
-                    y={200}
-                    width={50}
-                    height={50}
+                    x={0}
+                    y={0}
+                    width={width}
+                    height={height}
                     fill='white'
                     stroke='black'
-                    strokeWidth='.5'
+                    strokeWidth='5'
                 />
+
+                {/* Grid */}
+                {gridRects}
             </Layer>
         </Stage>
+        
     );
 }
 

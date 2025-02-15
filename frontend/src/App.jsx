9a36a627281746595d5d2af1193fc5fa535bee1e
andrew-adomaitis <theelderone02@gmail.com>
2025-02-15 18:35:25 -0500

    Working through customizing the wall sizes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,34 @@ import RoomGrid from './components/RoomGrid/RoomGrid.jsx';
 import ExportToPDF from './components/ExportToPDF/ExportToPDF.jsx';
 
 function App() {
+  let roomDataEntered = false;
+
+  let roomDetails = {
+    footPx: 30,
+    leftWallFeet: null,
+    topWallFeet: null,
+    rightWallFeet: null
+  }
+
+  const handleRoomDetails = (roomDetails) => {
+    const leftWallFeet = prompt("Enter the length of the left wall (in feet)");
+    const topWallFeet = prompt("Enter the length of the top wall (in feet)");
+    const rightWallFeet = prompt("Enter the length of the right wall (in feet)");
+  }
+
   return (
     <div>
       <ExportToPDF />
-      <RoomGrid
+      {!roomDataEntered 
+      ? <button onClick={handleRoomDetails}>
+        Enter Room Details
+      </button>
+      : <RoomGrid
         footPx={30}
         leftWallFeet={10}
         topWallFeet={10}
         rightWallFeet={15}
-      />
+      />}
     </div>
   )
 }

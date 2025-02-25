7f2f63e4073fecdd93deca1fb4ff7a865e6e1b3a
midasPrimaris <theelderone02@gmail.com>
2025-02-25 14:58:22 -0500

    Updated backend to add in inheritance structure

--- backend/object/models.py
+++ backend/object/models.py
@@ -1,15 +1,72 @@
 from django.db import models
 
-class Cabinet(models.Model):
-    name = models.CharField(max_length=100)
+class Object(models.Model):
+    """
+    **Abstract** base class for all objects in the system.\n
+    NOTE: All dimensions (position, dimensions, etc) are in *inches* not *pixels*.
 
-    # Cabinet dimensions are in inches
-    width = models.FloatField()
-    height = models.FloatField()
+    Attributes:
+        Public:
+            get_dimensions() - float[]: Returns the width and height of the object.\n
+            get_position() - int[]: Returns the x, y position of the object.\n
+            move_to(float x, float y) - void: Moves the object to a specified (x, y) position on the canvas.\n
 
-    # Cabinet's position on the canvas (in pixels)
-    position_x = models.FloatField(null=True, blank=True)
-    position_y = models.FloatField(null=True, blank=True)
+        Private:
+            _name - str: The name of the object.\n
+            _width - float: The width of the object (in inches).\n
+            _height - float: The height of the object (in inches).\n
+            _positionX - float: The x position of the object (in inches).\n
+            _positionY - float: The y position of the object (in inches).
 
-    def __str__(self):
-        return self.name
+    """
+    # This class is abstract so that Cabinet and Obstacle can inherit from it
+    class Meta:
+        abstract = True
+    
+    # Public fields
+    def get_dimensions(self):
+        """Returns the object's dimensions in inches (width, height)"""
+        return (self._width, self._height)
+
+    def get_position(self):
+        """Returns the object's (x, y) position in inches"""
+        return (self._position_x, self._position_y)
+    
+    def move_to(self, x, y):
+        """Moves the object to a new (x, y) position"""
+        self._position_x = x
+        self._position_y = y
+        return (self._position_x, self._position_y)
+
+    # Private fields, note: all dimension variables will be in inches
+    _name = models.CharField(max_length=100)
+    _width = models.FloatField(default=0.0)
+    _height = models.FloatField(default=0.0)
+    _position_x = models.FloatField(default=0.0)
+    _position_y = models.FloatField(default=0.0)
+
+
+class Cabinet(Object):
+    """
+    Represents a cabinet that can be placed in a room. Inherits from *Object* class.
+
+    This class uses all attributes defined in *Object* and adds 1 method - place_on_canvas
+
+    Typical usage example:
+
+        cabinet = Cabinet
+    """
+    
+    def __init__(self, *args, place_x=None, place_y=None, **kwargs):
+        """Creates a Cabinet with initial coordinates."""
+        # If provided, assign the coordinates to the appropriate fields
+        if place_x is not None:
+            kwargs["position_x"] = place_x
+        if place_y is not None:
+            kwargs["position_y"] = place_y
+        
+        super().__init__(*args, **kwargs)
+
+    def place_on_canvas(self, x, y):
+        """Moves a cabinet to a new position"""
+        self.move_to(x, y)
--- backend/object/views.py
+++ backend/object/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Cabinet
-from .utils import place_cabinet_on_canvas
 
 # Create your views here.
 @api_view(['POST'])
@@ -11,8 +10,11 @@ def place_cabinet(request):
     API endpoint to place a cabinet at a specified x and y position
     """
 
+    print("Request data:", request.data)
+
     # Gather the needed data from the request (will be used to call place_cabinet from utils)
     cabinet_data = request.data.get('cabinet')
+    print('cabinet data:', cabinet_data)
     x = request.data.get('x')
     y = request.data.get('y')
 
@@ -20,14 +22,29 @@ def place_cabinet(request):
         return Response({'error': 'not all parameters entered for place_cabinet'}, status=400)
     
     try:
+        print('about to place cabinet')
         cabinet = Cabinet(
-            name=cabinet_data['name'],
-            width=cabinet_data['width'],
-            height=cabinet_data['height']
+            _name=cabinet_data['name'],
+            _width=cabinet_data['width'],
+            _height=cabinet_data['height'],
+            _position_x=float(x),
+            _position_y=float(y)
         )
+        print('placed cabinet')
+        cabinet.save()
+        print('saved cabinet')
     except KeyError as e:
         return Response({'error:' f'Missing key in cabinet data: {str(e)}'}, status=400)
+    except Exception as e:
+        print(f"Exception occurred: {str(e)}")
+        return Response({'error:', str(e)}, status=500)
 
-    placed_cabinet = place_cabinet_on_canvas(cabinet, float(x), float(y))
+    response_data = {
+        'name': cabinet._name,
+        'width': cabinet._width,
+        'height': cabinet._height,
+        'position_x': cabinet._position_x,
+        'position_y': cabinet._position_y
+    }
 
-    return Response({'placed_cabinet': placed_cabinet})
+    return Response({'placed_cabinet': response_data})

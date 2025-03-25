400d64954e860530894e8aadb6fa4f08d2a3666e
midasPrimaris <theelderone02@gmail.com>
2025-03-25 12:31:26 -0400

    Added POST method generating cabinet layout

--- backend/object/views.py
+++ backend/object/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models.cabinet import Cabinet
+from .models.wall import Wall
 
 @api_view(['POST'])
 def place_cabinet(request):
@@ -49,3 +50,39 @@ def place_cabinet(request):
     }
 
     return Response({'placed_cabinet': response_data})
+
+@api_view(['POST'])
+def generate_wall(request):
+    """
+    Endpoint to test cabinet layout generation.
+    Expected JSON payload:
+    {
+        "width": <number>,
+        "generation": "b1" | "b2" | "b3" | "u1" | "u2" | "u3"
+    }
+    """
+    data = request.data
+    width = data.get("width")
+    generation = data.get("generation", "b1") # Default to gen b1
+
+    if width is None:
+        return Response({"error": "Width is required"}, status=400)
+    
+    try:
+        wall = Wall(width=width)
+        if generation == "b1":
+            wall.generation_b1()
+            layout = {"bases": wall.bases}
+        elif generation == "b2":
+            wall.generation_b2()
+            layout = {"bases": wall.bases}
+        elif generation == "b3":
+            wall.generation_b3()
+            layout = {"bases": wall.bases}
+        else:
+            return Response({"error": "Invalid generation type."}, status=400)
+        # TODO implement uppers
+
+        return Response({"layout": layout})
+    except Exception as e:
+        return Response({"error": str(e)}, status=500)

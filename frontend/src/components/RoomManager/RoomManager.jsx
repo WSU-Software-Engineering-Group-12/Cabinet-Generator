9a79f688a34ac2505a2cd67bd292a5367d253210
midasPrimaris <theelderone02@gmail.com>
2025-03-30 10:27:41 -0400

    Beginning Styling

--- frontend/src/components/RoomManager/RoomManager.jsx
+++ frontend/src/components/RoomManager/RoomManager.jsx
@@ -3,15 +3,17 @@ import RoomGrid from '../RoomGrid/RoomGrid.jsx';
 import ExportToPDF from '../ExportToPDF/ExportToPDF.jsx';
 import Wall from "../Wall/Wall.jsx";
 import { Group, Layer, Stage } from "react-konva";
-import { defaultGridOffset } from "../../../utils/globalVars.js";
+import { defaultGridOffset, defaultInchPx } from "../../../utils/globalVars.js";
+import './RoomManager.css';
+import CabinetKey from '../CabinetKey/CabinetKey.jsx'
 
 const RoomManager = () => {
     const [roomDetails, setRoomDetails] = useState(null);
 
     const handleRoomDetails = () => {
-        const leftWallFeet = parseFloat(prompt("Enter the length of the left wall (in feet)"), 10);
-        const topWallFeet = parseFloat(prompt("Enter the length of the top wall (in feet)"), 10);
-        const rightWallFeet = parseFloat(prompt("Enter the length of the right wall (in feet)"), 15);
+        const leftWallFeet = parseFloat(prompt("Enter the length of the left wall (in inches)"), 10);
+        const topWallFeet = parseFloat(prompt("Enter the length of the top wall (in inches)"), 10);
+        const rightWallFeet = parseFloat(prompt("Enter the length of the right wall (in inches)"), 15);
     
         // Ensure that no invalid characters have been passed
         if (
@@ -24,51 +26,68 @@ const RoomManager = () => {
         }
     
         setRoomDetails({
-          footPx: 10,
+          footPx: defaultInchPx,
           leftWallFeet,
           topWallFeet,
           rightWallFeet
         });
     }
+
+    const getStageHeight = () => {
+        if(!roomDetails) return window.innerHeight;
+
+        const leftWallHeight = roomDetails.leftWallFeet * roomDetails.footPx;
+        const rightWallHeight = roomDetails.rightWallFeet * roomDetails.footPx;
+
+        return Math.max(leftWallHeight, rightWallHeight);
+    }
+
     return (
-        <div>
+        <div className="room-manager">
             {!roomDetails ? (
-                <>
+                <div className="intro-text">
                     <h1>Welcome to CabineXt</h1>
                     <p>Generate a cabinet layout tailored specifically to your own room!</p>
                     <button onClick={handleRoomDetails}>Enter Your Room&apos;s Details</button>
-                </>
+                </div>
                 
             ) : (
-                <>
-                    <h2>Room Layout</h2>
-                    <Stage width={5000} height={5000}>
-                        <Layer>
-                            <Group x={defaultGridOffset} y={defaultGridOffset}>
-                                <RoomGrid 
-                                    leftWallFeet={roomDetails.leftWallFeet}
-                                    topWallFeet={roomDetails.topWallFeet}
-                                    rightWallFeet={roomDetails.rightWallFeet}
-                                />
-                                <Wall 
-                                    lengthFeet={roomDetails.leftWallFeet} 
-                                    orientation="left" 
-                                />
-                                <Wall 
-                                    lengthFeet={roomDetails.topWallFeet} 
-                                    orientation="top" 
-                                />
-                                <Wall 
-                                    lengthFeet={roomDetails.rightWallFeet}
-                                    orientation="right" 
-                                    offset={roomDetails.topWallFeet} 
-                                />
-                            </Group>
-                        </Layer>
-                        
-                    </Stage>
-                    <ExportToPDF />
-                </>
+                <div className="flex-box">
+                    <div className='flex-item'>
+                        <CabinetKey />
+                        <ExportToPDF />
+                    </div>
+
+                    <div className='flex-item'>
+                        <h2>Room Layout</h2>
+                        <Stage width={window.innerWidth} height={getStageHeight()}>
+                            <Layer>
+                                <Group x={defaultGridOffset} y={defaultGridOffset}>
+                                    <RoomGrid 
+                                        leftWallFeet={roomDetails.leftWallFeet}
+                                        topWallFeet={roomDetails.topWallFeet}
+                                        rightWallFeet={roomDetails.rightWallFeet}
+                                    />
+                                    <Wall 
+                                        lengthFeet={roomDetails.leftWallFeet} 
+                                        orientation="left" 
+                                    />
+                                    <Wall 
+                                        lengthFeet={roomDetails.topWallFeet} 
+                                        orientation="top" 
+                                    />
+                                    <Wall 
+                                        lengthFeet={roomDetails.rightWallFeet}
+                                        orientation="right" 
+                                        offset={roomDetails.topWallFeet} 
+                                    />
+                                </Group>
+                            </Layer>
+                            
+                        </Stage>
+                    </div>
+                    
+                </div>
             )}
         </div>
     );
